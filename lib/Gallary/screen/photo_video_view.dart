0ae4831b9e231059356b80b98d2aa73825a5b0cc
Neel Gediya <nkgediya206@gmail.com>
2025-10-01 17:00:44 +0530

    Refactor PhotoVideoView to manage video playback in initState and change to StatefulWidget

--- lib/Gallary/screen/photo_video_view.dart
+++ lib/Gallary/screen/photo_video_view.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:video_player/video_player.dart';
 
-class PhotoVideoView extends StatelessWidget {
+class PhotoVideoView extends StatefulWidget {
   final List<AssetEntity> data;
   final int index;
   const PhotoVideoView({super.key, required this.data, required this.index});
 
   @override
+  State<PhotoVideoView> createState() => _PhotoVideoViewState();
+}
+
+class _PhotoVideoViewState extends State<PhotoVideoView> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.data[widget.index].type == AssetType.video) {
+      widget.data[widget.index].file.then((file) {
+        if (file != null) {
+          _controller = VideoPlayerController.file(file)
+            ..initialize().then((_) {
+              setState(() {});
+              _controller.play();
+            });
+        }
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     
     return PageView.builder(
-      itemCount: data.length,
-       controller: PageController(initialPage: index),
+      itemCount: widget.data.length,
+       controller: PageController(initialPage: widget.index),
       itemBuilder: (context, indexx) {
-        return FutureBuilder(future: data[indexx].originBytes, builder: (context, snapshot) {
+        return FutureBuilder(future: widget.data[indexx].originBytes, builder: (context, snapshot) {
       if(snapshot.connectionState == ConnectionState.waiting){
         return Center(
           child: CircularProgressIndicator(),
